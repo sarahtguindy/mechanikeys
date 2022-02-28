81394a1eecf90d181efb5b85d69b5bc975a0c707
Sarah Guindy <sarahtg98@gmail.com>
2022-02-28 15:01:55 -0500

    Admin product list, add style to navbar

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -14,9 +14,9 @@ const Header = () => {
 
   return (
     <header>
-      <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
+      <Navbar bg="primary" variant="dark" expand="lg" collapseOnSelect>
         <Container>
-          <LinkContainer to="/">
+          <LinkContainer to="/" className="brand">
             <Navbar.Brand>ProShop</Navbar.Brand>
           </LinkContainer>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
